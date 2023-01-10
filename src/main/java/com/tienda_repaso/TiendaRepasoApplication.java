7418528f0c0b761dc78943ed7c387e85bd167776
jubargra <jubargra@gmail.com>
2023-01-09 21:56:03 -0600

    Se crea el proyecto.

---
+++ src/main/java/com/tienda_repaso/TiendaRepasoApplication.java
@@ -0,0 +1,13 @@
+package com.tienda_repaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaRepasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaRepasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tienda_repaso/TiendaRepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_repaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaRepasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
